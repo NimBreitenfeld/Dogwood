3b8d84b0af514e10435e8c95f4bae75514d92a6d
naomikb <naomikb@live.unc.edu>
2016-07-13 12:14:40 -0400

    new file

---
+++ pokemon_survey.sh
@@ -0,0 +1,15 @@
+# What is your name?
+echo "What is your name?"
+read NAME
+#Do you play Pokemon Go? (y/n)
+echo "Do you play Pokemon Go? (y/n)"
+read PLAY
+#Are you on or would you like to be on Team Mystic, Valor, or Instint? (one of the three)
+echo "Are you on or would you like to be on Team Mystic, Valor, or Instint? Choose ONLY ONE."
+read TEAM
+#What is your favorite pokemon? (pokemon)
+echo "What is your favorite pokemon?"
+read FAVORITE
+#How many pokemon games have you played in the franchise? (number)
+echo "How many pokemon games have you played in the franchise?"
+read NUMBER
