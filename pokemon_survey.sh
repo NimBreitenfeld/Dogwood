659727940ae0e8956cd4768aea82204f13acfe36
naomikb <naomikb@live.unc.edu>
2016-07-13 12:08:37 -0400

    fixing things

--- pokemon_survey.sh
+++ pokemon_survey.sh
@@ -4,16 +4,16 @@
 echo "What is your name?"
 read NAME
 #Do you play Pokemon Go? (y/n)
-echo "Do you play Pokemon Go? (y/n)""
+echo "Do you play Pokemon Go? (y/n)"
 read PLAY
 #Are you on or would you like to be on Team Mystic, Valor, or Instint? (one of the three)
-echo "Are you on or would you like to be on Team Mystic, Valor, or Instint? (one of the three)"
+echo "Are you on or would you like to be on Team Mystic, Valor, or Instint? Choose ONLY ONE."
 read TEAM
 #What is your favorite pokemon? (pokemon)
-echo "What is your favorite pokemon? (pokemon)"
+echo "What is your favorite pokemon?"
 read FAVORITE
 #How many pokemon games have you played in the franchise? (number)
-echo "How many pokemon games have you played in the franchise? (number)"
+echo "How many pokemon games have you played in the franchise?"
 read NUMBER
 
-TIME`date
+# TIME`date
